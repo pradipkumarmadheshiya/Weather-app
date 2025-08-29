74d335ed344f03e1a54643638a76b46dacbddf53
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-08-29 17:57:26 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
           />
           <button
             onClick={getWeather}
-            className="px-4 py-2 bg-blue-500 text-white rounded-xl hover:bg-blue-600 transition"
+            className="px-4 py-2 bg-blue-500 text-white rounded-xl hover:bg-blue-600 transition cursor-pointer"
           >
             Search
           </button>
